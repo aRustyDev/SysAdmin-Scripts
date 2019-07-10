9a8b23557ae61ef57c7b8058a4a76944a63277b4
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-07-10 11:21:00 -0500

    Update addToPATH_Global.ps1

--- WorkStationSetup/addToPATH_Global.ps1
+++ WorkStationSetup/addToPATH_Global.ps1
@@ -2,4 +2,9 @@
 $oldPATH = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
 $newPATH = "$oldpath;c:\path\to\folder"
 #May want to add in a admin sign-off/checkpoint to ensure the PATH Var is correct
+#Try -- ($env:path).split(";")
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+
+
+#Easy way to read the PATH Variables
+#($env:path).split(";")
