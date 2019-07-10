7297a2ff0be1dc61ac2ac36f5dae26b2a9191c02
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-07-10 11:19:46 -0500

    Update addToPATH_Global.ps1

--- WorkStationSetup/addToPATH_Global.ps1
+++ WorkStationSetup/addToPATH_Global.ps1
@@ -1,2 +1,5 @@
+#Get PATH Variables, @ the Global Level, shove into an "Old Variable" so we can append it.
 $oldPATH = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
 $newPATH = "$oldpath;c:\path\to\folder"
+#May want to add in a admin sign-off/checkpoint to ensure the PATH Var is correct
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
