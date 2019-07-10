228b288c0d66f809450f2c0ec3cfd4e7a4f31a34
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-07-10 10:15:18 -0500

    Create searchEventLogs.ps1

---
+++ EventLogs/searchEventLogs.ps1
@@ -0,0 +1,32 @@
+#Script mostly pulled from Microsoft Documentation, slight edit to let admin search by variable string.
+#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/write-progress?view=powershell-6
+
+#Query User for Event String to search for
+$targEvent = Read-Host -Prompt "What event do you want to look for?: "
+$eventStr = "*" + $targEvent + "*"
+# Use Get-EventLog to get the events in the System log and store them in the $Events variable.
+$Events = Get-EventLog -LogName system
+# Pipe the events to the ForEach-Object cmdlet.
+$Events | ForEach-Object -Begin {
+    # In the Begin block, use Clear-Host to clear the screen.
+    Clear-Host
+    # Set the $i counter variable to zero.
+    $i = 0
+    # Set the $out variable to a empty string.
+    $out = ""
+} -Process {
+    # In the Process script block search the message property of each incoming object for "bios".
+    if($_.message -like $eventStr)
+    {
+        # Append the matching message to the out variable.
+        $out=$out + $_.Message
+    }
+    # Increment the $i counter variable which is used to create the progress bar.
+    $i = $i+1
+    # Use Write-Progress to output a progress bar.
+    # The Activity and Status parameters create the first and second lines of the progress bar heading, respectively.
+    Write-Progress -Activity "Searching Events" -Status "Progress:" -PercentComplete ($i/$Events.count*100)
+} -End {
+    # Display the matching messages using the out variable.
+    $out
+}
