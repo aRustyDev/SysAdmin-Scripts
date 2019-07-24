37b0e7341025c33991b96b756b1cb9c18f5cb2e1
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-07-24 18:52:04 -0500

    Add files via upload

---
+++ WorkStationSetup/newWorkstationSetup/NewWorkStationSetup.bat
@@ -0,0 +1,4 @@
+::Set Current Dir to D:\
+d:\
+::Open PowerShell, bypass the Execution Policy, & Run the Workstation setup script
+PowerShell.exe -ExecutionPolicy Bypass -File ".\scripts\NewWorkstationSetup_noAppX.ps1"
