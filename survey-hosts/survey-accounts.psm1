8ee9b45e4c1f71eab25a4a3762c2ffb27fd1f9af
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-09-08 15:05:37 -0500

    added base version of Survey-Accounts

---
+++ survey-hosts/survey-accounts.psm1
@@ -0,0 +1,21 @@
+function survey-accounts {
+    param (
+        # Parameter help description
+        [Parameter()]
+        [string[]]
+        $targets,
+
+        # Parameter help description
+        [Parameter()]
+        [pscredential]
+        $creds
+    )
+    begin {
+        if(!$creds){ $creds = Get-Credential}
+    }
+    process {
+        Invoke-Command -ComputerName $targets -Credential $creds -ScriptBlock {
+            Get-wmiobject win32_useraccount | Select-Object AccountType, Name, LocalAccount, Domain, SID
+        }
+    }
+}
