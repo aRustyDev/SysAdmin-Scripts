7e45340af0553df0b0e3043664e1a638e53371f5
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-09-08 16:01:27 -0500

    Added logic for filtering by subnet/os

--- survey-hosts/survey-processes.psm1
+++ survey-hosts/survey-processes.psm1
@@ -25,6 +25,22 @@ function survey-processes {
     )
     begin {
         if(!$creds){ $creds = Get-Credential}
+        #$targets = Import-Csv -Path $env:WINHOSTS_CSV
+
+        $i = 0
+        foreach ($arg in $args) {
+            if($arg -imatch "-subnet"){
+                $targets = $targets | Where-Object {$_.subnet -match $args[$i+1]}
+                $i++
+                continue
+            }
+            if($arg -imatch "-os"){
+                $targets = $targets | Where-Object {$_.os -match $args[$i+1]}
+                $i++
+                continue
+            }
+            $i++
+        }
     }
     process {
         Invoke-Command -ComputerName $targets -Credential $creds -ScriptBlock {
