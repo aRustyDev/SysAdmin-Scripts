0d9584c0fec945a772cc3436124f24316d264394
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-09-08 15:25:18 -0500

    added base version of survey-processes

---
+++ survey-hosts/survey-processes.psm1
@@ -0,0 +1,38 @@
+function survey-processes {
+    param (
+        [CmdletBinding]
+
+        # Parameter help description
+        [Parameter()]
+        [string[]]
+        $targets,
+
+        # Parameter help description
+        [Parameter()]
+        [pscredential]
+        $creds
+    )
+    begin {
+        if(!$creds){ $creds = Get-Credential}
+    }
+    process {
+        Invoke-Command -ComputerName $targets -Credential $creds -ScriptBlock {
+            Get-wmiobject win32_process | Select-Object Name, ProcessID, Path, Commandline, `
+                @{ #Custom Select Number 1
+                    Name="Hash"; 
+                    Expression={
+                        if($_.path){
+                            (certutil.exe -hashfile $_.FullName SHA256)[1] -replace " ", ""
+                        }else { "" }
+                    }
+                }, `
+                @{ #Custom Select Number 2
+                    Name="Process_Owner"; 
+                    Expression={
+                        $_.getowner().domain + "\" + $_.getowner().user
+                    }     
+                } 
+        }
+    }
+    
+}
